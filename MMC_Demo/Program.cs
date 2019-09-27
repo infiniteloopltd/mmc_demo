6401bb4fe601fef76e46b8f9c90d781017ad80d0
Fiach Reid <fiach.reid@gmail.com>
2019-09-27 16:49:40 +0100

    Initial Commit

---
+++ MMC_Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MMC_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MMC Demo");
+            Console.WriteLine("--------");
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Please call as follows; MMC_Demo.exe <username> <registration> <state>");
+            }
+
+            var username = args[0];
+            var reg = args[1];
+            var state = args[2];
+
+            var api = new regcheck.CarReg();
+
+            var data = api.CheckUSA(reg, state, username);
+
+            Console.WriteLine(data.vehicleJson);
+
+
+
+
+        }
+    }
+}
